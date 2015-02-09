2816b8d898aef13bc647eb6721093fa3db6120ee
Vadim Lobanov <vadim@lobanov.pw>
2015-02-09 21:26:35 +0300

    add parse-phone function

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.googlecode.libphonenumber/libphonenumber "7.0.2"]])
--- src/libphonenumber/core.clj
+++ src/libphonenumber/core.clj
@@ -1,6 +1,36 @@
-(ns libphonenumber.core)
+(ns libphonenumber.core
+  (:import [com.google.i18n.phonenumbers PhoneNumberUtil
+                                         PhoneNumberUtil$PhoneNumberFormat
+                                         PhoneNumberUtil$PhoneNumberType
+                                         NumberParseException]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def format-e164 PhoneNumberUtil$PhoneNumberFormat/E164)
+
+(def phone-types 
+  {PhoneNumberUtil$PhoneNumberType/FIXED_LINE :fixed-line
+   PhoneNumberUtil$PhoneNumberType/MOBILE :mobile
+   PhoneNumberUtil$PhoneNumberType/FIXED_LINE_OR_MOBILE :fixed-line-or-mobile
+   PhoneNumberUtil$PhoneNumberType/TOLL_FREE :toll-free
+   PhoneNumberUtil$PhoneNumberType/PREMIUM_RATE :premium-rate
+   PhoneNumberUtil$PhoneNumberType/SHARED_COST :shared-cost
+   PhoneNumberUtil$PhoneNumberType/VOIP :voip
+   PhoneNumberUtil$PhoneNumberType/PERSONAL_NUMBER :personal-number
+   PhoneNumberUtil$PhoneNumberType/PAGER :pager
+   PhoneNumberUtil$PhoneNumberType/UAN :uan
+   PhoneNumberUtil$PhoneNumberType/VOICEMAIL :voicemail
+   PhoneNumberUtil$PhoneNumberType/UNKNOWN :unknown})
+
+(defn parse-phone [^String phone ^String country-code]
+  (let [number-util (PhoneNumberUtil/getInstance)]
+    (try
+      (let [parsed (.parse number-util phone country-code)
+            number-type (get phone-types
+                             (.getNumberType number-util parsed)
+                             :unknown)
+            is-valid (if (.isValidNumber number-util parsed)
+                         :valid
+                         :invalid)
+            formatted (.format number-util parsed format-e164)]
+        [is-valid {:type number-type
+                   :formatted formatted}])
+      (catch NumberParseException e [:error (str e)]))))
