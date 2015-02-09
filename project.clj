41445d3853a650569a57e6f87ceb80bcd94e9ff1
Vadim Lobanov <vadim@lobanov.pw>
2015-02-09 21:39:02 +0300

    bumb version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.vlobanov/libphonenumber "0.1.0-SNAPSHOT"
+(defproject me.vlobanov/libphonenumber "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
