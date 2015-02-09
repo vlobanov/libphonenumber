8d5d89c5060e5ec9b77febae78012d6f2f2c4c11
Vadim Lobanov <vadim@lobanov.pw>
2015-02-09 20:31:12 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to libphonenumber
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject libphonenumber "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.googlecode.libphonenumber/libphonenumber "7.0.2"]])
