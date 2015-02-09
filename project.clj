d88626c0df8c799fb6c33c341439f0d838a15ce6
Vadim Lobanov <vadim@lobanov.pw>
2015-02-09 21:27:39 +0300

    add namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libphonenumber "0.1.0-SNAPSHOT"
+(defproject me.vlobanov/libphonenumber "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
