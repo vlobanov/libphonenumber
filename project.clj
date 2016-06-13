f340f464bc8651afccaf60906efd134602dffc10
Vadim Lobanov <vadim@lobanov.pw>
2016-06-13 21:33:52 +0200

    use 7.4.2 of libphonenumber

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject me.vlobanov/libphonenumber "0.1.1-SNAPSHOT"
+(defproject me.vlobanov/libphonenumber "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.googlecode.libphonenumber/libphonenumber "7.0.2"]])
+                 [com.googlecode.libphonenumber/libphonenumber "7.4.2"]])
