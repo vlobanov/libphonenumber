fbb9305b8789133c782dceadde9b8cedd24e563c
jinjun <jinjun@audiencerepublic.com>
2023-01-16 16:06:03 +1100

    bump google libphonenumber version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.googlecode.libphonenumber/libphonenumber "8.10.18"]])
+                 [com.googlecode.libphonenumber/libphonenumber "8.13.4"]])
